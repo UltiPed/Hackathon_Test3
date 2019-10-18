05c75679d956a99e75237b9ad9138943485bf338
UltiPed <bbkkwong1998@gmail.com>
2019-10-19 04:01:56 +0800

    testing rest API

---
+++ App1/App1/Data/RestService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace App1.Data
+{
+    public class RestService
+    {
+        public HttpClient client;
+
+        public RestService()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("gateway.sandbox.tapngo.com.hk");
+            client.MaxResponseContentBufferSize = 256000;
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+        }
+
+        public async Task SinglePaymentOrRequestRecurrentToken(String appId, String APIKey, String publicKey)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/web/payments");
+            request.Headers.Add("Content-Type", "application/x-www-form - urlencoded");
+            var postData = new List<KeyValuePair<String, String>>();
+            postData.Add(new KeyValuePair<string, string>("appId", appId));
+            postData.Add(new KeyValuePair<string, string>("APIKey", APIKey));
+            postData.Add(new KeyValuePair<string, string>("publicKey", publicKey));
+        }
+    }
+}
