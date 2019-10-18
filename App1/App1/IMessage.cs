4fd818bf47f19ca2632ec7414b69c2b466ed1fce
UltiPed <bbkkwong1998@gmail.com>
2019-10-18 20:58:30 +0800

    files to upload

---
+++ App1/App1/IMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public interface IMessage
+    {
+        string run(string Url,string json);
+
+        string bowlingJson(string player1,string player2);
+
+
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        String result = "";
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+          
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            IMessage platform = Xamarin.Forms.DependencyService.Get<IMessage>();
+            string json = platform.bowlingJson("Jesse", "Jake");
+
+            result = await Task.Run(() =>
+                platform.run("https://raw.github.com/square/okhttp/master/README.md",json)
+            );
+
+            result = "";
+        }
+    }
+}
