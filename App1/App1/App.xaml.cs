03a45d34324210dfe6264f99b53c47d8807a059a
AlanChu-OU <g1186214@study.ouhk.edu.hk>
2019-10-19 02:31:03 +0800

    commit

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MenuShell();
         }
 
         protected override void OnStart()
